4774653fcff3e8cadb5a7d410cfb6d22ac624a07
Christian <cfortuna1994@gmail.com>
2021-09-17 18:50:15 +0100

    New function blind-auction

--- day09/blind-auction/main.py
+++ day09/blind-auction/main.py
@@ -1,12 +1,43 @@
 import art
+import os
+clear = lambda: os.system('cls')
+
 #from replit import clear
 #HINT: You can call clear() to clear the output in the console.
+bids = []
+print(art.logo)
 
-Name = input("What is your name?")
-Bid = input("What is your bid?")
 
-Bids = {}
-Bids["Name"] = Name
-Bids["Bid"] = Bid
+def Getting_bids():
+    Name = input("What is your name?")
+    Bid = input("What is your bid?")
+    Person = {}
+    Person["Name"] = Name
+    Person["Bid"] = Bid
+    bids.append(Person)   
 
-print(Bids)
+def getting_higher_bid():
+    higher = 0
+    for element in bids:
+        if int(element["Bid"]) > int(higher):
+            higher = element["Bid"]
+            winner = {}
+            winner["Name"] = element["Name"]
+            winner["Bid"] = element["Bid"]
+    print(f"The winner is: {winner}")
+
+End_of_game = False
+
+while(End_of_game == False):
+    Getting_bids()
+    answer = int(input("Is there other user? (1 = Yes | 2 = No): "))
+    if answer == 2:
+        End_of_game = True
+        clear()
+        getting_higher_bid()
+
+
+
+
+
+    
