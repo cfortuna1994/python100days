2a33e16db5e549b5719fb69eebff061228025add
Christian Magalhaes <it@uniformal.ie>
2021-09-17 17:20:51 +0100

    Blind Auction 1.0

---
+++ day09/blind-auction/main.py
@@ -0,0 +1,12 @@
+import art
+#from replit import clear
+#HINT: You can call clear() to clear the output in the console.
+
+Name = input("What is your name?")
+Bid = input("What is your bid?")
+
+Bids = {}
+Bids["Name"] = Name
+Bids["Bid"] = Bid
+
+print(Bids)
